15d45827cf82594c7dd3528fcc78ff40a15519f8
ElianHD <147518386+ElianHD@users.noreply.github.com>
2025-11-30 12:11:15 +0100

    Update Install_Dockeur.sh
    Mise a jour du scripts

--- Install_Dockeur.sh
+++ Install_Dockeur.sh
@@ -3,14 +3,15 @@
 set -e
 apt update
 apt upgrade
-apt install git -y
+echo "Update && Upgrade des Paquets"
 apt install curl -y
 apt install ca-certificates -y
+echo "Installation DOCKEUR sur le dockeur.com"
 curl https://get.docker.com | bash
 apt install docker-compose -y
 systemctl enable --now docker
 systemctl start docker
+apt install containerd.io=1.7.28-1~debian.12~bookworm -y
 echo "Terminer "
 
-apt install containerd.io=1.7.28-1~debian.12~bookworm -y
-docker run hello-world
+
